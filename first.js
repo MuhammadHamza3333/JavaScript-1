6d26203909519540e8315c030bb692850483db6d
MuhammadHamza3333 <164262065+MuhammadHamza3333@users.noreply.github.com>
2024-03-24 14:55:27 -0700

    Add files via upload

---
+++ first.js
@@ -0,0 +1,225 @@
+// console.log("Welcome to javascript");
+// console.log("Hafiz Hamza Atiq");
+// console.log("I love Java Script");
+
+
+
+
+
+
+
+// variables are containers for data
+// variables ke name me space nahi ho sakti warna ye eror show kare ga!
+// variables me dolor/$ sing add ker  sakte hai!
+// variables me underscore/_ sing add ker  sakte hai!
+// variables me or Numbers b add ker  sakte hai!
+// only a letter underscore $ should be first character
+// first word ka letter small ho ga or second word ka capital to hum ise camil case kehte hai!
+
+
+
+
+// fullname = "hafiz Hamza Atiq";
+// console.log(fullname);
+// age = 19;
+// console.log(age);
+// price = 1520.41411;
+// console.log(price);
+// // null pata to hai is ander kya hai lekin ye ak jhali value hai?
+// x = 'null';
+// console.log(x);
+// // undefined ka matlab pata nahi is ke under kya hai!
+// y = 'undefined'
+// console.log(y);
+
+// bolean value ke two types hote ye to true/false
+// three keyword we use in java script in varaibles 1 let, 2 var, 3 const,
+
+
+
+// var fullName = "Hafiz Hamza Atiq";
+// console.log(fullName);
+// var age = 19;
+// console.log(age);
+
+
+// but is a good programer hum var ko kabhi use nahi kare ge as a developer
+
+// let
+
+// let fullName = 'Hafiz Hamza Atiq';
+// console.log(fullName);
+// let age = 19;
+// console.log(age);
+// let totalPrize = 15451515452;
+// console.log(totalPrize);
+
+
+
+
+
+
+// {
+//     let a = 'hamza';
+//     console.log(a);
+// }
+// This is a block
+
+// const ka matlab ye hai hum ne fix ker diya value ko ab change nahi ker sakte
+// let suppose
+// const a = 18;
+// a = 19;
+// console.log(a);
+
+
+// Data Types of Js
+// 1 String
+// 2 Number
+// 3 Bolean
+// 4 undefined
+// 5 Null
+// 6 Bigint
+// 7 Symbol
+
+// primitive Data types jo ke bilkul fixed hote hai! rk language ke ander
+// there is 7 primitive data types in  js
+
+// //string
+//  let fullName = "Hafiz Hamza Atiq";
+//  console.log(typeof(fullName));
+//  //Number
+//  let age= 353
+//  console.log(typeof(age));
+// //  // boolean
+//  let isAvailable = true
+//  console.log(typeof( isAvailable));
+//  // undefined
+//  let a;
+//   console.log(typeof(a)); 
+//   // null
+//   let b = null
+//   console.log(typeof(b));
+//   // Bigint
+//   let x = BigInt(1234)
+//   console.log(x);
+//    console.log(typeof(x)); 
+//    // symbol
+//    let y = Symbol(243353)
+//   console.log(y);
+
+//   console.log(typeof(y));
+
+// / Non primitive data types
+// 1 Objects //// =====> it is collection of all defferent  values
+// object
+// const student = {
+//  fullName : "Hafiz Hamza Atiq" ,
+//  age: 16 ,
+// isAvailable: true ,
+
+
+// }
+// student ['age'] = student ['age'] +1
+// console.log(student .age);
+//  const information ={
+//     fullName : 'Hafiz Hamza Atiq',
+//     age: 17,
+//     isAvailaible: true,
+//     class: "Matric"
+// }
+//  console.log( information);
+//  information["age"]  = information["age"] + 34
+//  information["name"] = 'kamil';
+//  console.log(information.name)
+
+
+
+
+// const student ={
+//     fullName: "Hafiz Hamza Atiq",
+//     age: 1233,
+//     isAvailable: true,
+//     marks: 734676,
+
+
+// }
+// student["age"]  = student["age"] +1;
+// student["fullName"] = "Kamil Atiq";
+// student["isAvailable"]= false;
+// console.log(student.isAvailable);
+
+
+
+// const varaibles ke ander Nahi change ker sakte
+// lekin key word ko change ker sakte hai 
+// let ko update ker sakte hai
+// const ko update nahi ker sakte
+// const ke ander ager object ho to us ki keys ko change ker sakte hai
+// practicle
+// objects
+// const profile ={
+//     fullName: " Shradda Khapra",
+//     post:123,
+//     followers: 23233,
+//     following: 4,
+//     userName:"@ Shradda Khapra",
+//     isfollow: true,
+
+
+// }
+// console.log(profile);
+
+
+
+
+
+//  const information ={
+// fullName: "Hafiz Hamza Atiq",
+// userName:"@hatiq",
+// Followers:442,
+// following:3213,
+// isAvailable:true
+//  }
+//  information["Followers"] =information["Followers"]+3;
+//  information["fullName"] = "Kamil";
+//  console.log(information.fullName);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
